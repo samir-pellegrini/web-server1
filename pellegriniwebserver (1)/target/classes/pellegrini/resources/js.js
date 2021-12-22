affe27723bb1e3519dbe32ec8d50a71e77fc8357
samir-pellegrini <73885481+samir-pellegrini@users.noreply.github.com>
2021-12-22 13:33:29 +0100

    web-server

---
+++ pellegriniwebserver (1)/target/classes/pellegrini/resources/js.js
@@ -0,0 +1,6 @@
+onsole.clear()
+
+// const node = document.getElementById('id')
+// const node = document.getElementsByClassName('class')
+// const nodes = document.querySelectorAll('div')
+// window.addEventListener()
